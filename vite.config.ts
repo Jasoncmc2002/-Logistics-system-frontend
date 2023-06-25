b8c395e95e549c1ba5676395bf28e85d663a30ae
单炟崴 <2795966208@qq.com>
2023-06-25 10:02:47 +0800

    feat: 1.01 添加了order界面

--- vite.config.ts
+++ vite.config.ts
@@ -44,7 +44,7 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
       proxy: {
         // 反向代理解决跨域
         [env.VITE_APP_BASE_API]: {
-          target: "http://10.25.37.172:8991",
+          target: "http://10.25.36.182:8991",
           // target: "http://vapi.youlai.tech", // 线上接口地址
           // target: 'http://localhost:8989',  // 本地接口地址 , 后端工程仓库地址：https://gitee.com/youlaiorg/youlai-boot
           changeOrigin: true,
