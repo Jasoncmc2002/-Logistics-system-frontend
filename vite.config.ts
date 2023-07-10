9f124afc315592b6c227f2a9f61d9ee11e1382b7
自由散漫惯 <2946476452@qq.com>
2023-07-10 11:31:20 +0800

    feat: bi回归

--- vite.config.ts
+++ vite.config.ts
@@ -76,7 +76,7 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
       proxy: {
         // 反向代理解决跨域
         [env.VITE_APP_BASE_API]: {
-          target: "http://10.25.36.178:10010",
+          target: "http://192.168.43.190:10010",
           // target: "http://vapi.youlai.tech", // 线上接口地址
           //target: 'http://localhost:8989',  // 本地接口地址 , 后端工程仓库地址：https://gitee.com/youlaiorg/youlai-boot
           changeOrigin: true,
