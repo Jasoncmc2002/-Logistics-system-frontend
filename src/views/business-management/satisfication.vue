f3be01d83fd09b1e0eaadee2184078ebab0b7419
cmc <2634622798@qq.com>
2023-07-11 16:08:04 +0800

    feat: 数据分析文件生成

--- src/views/business-management/satisfication.vue
+++ src/views/business-management/satisfication.vue
@@ -23,8 +23,7 @@ import { ReceiptPageVO, ReceiptQuery, ReceiptForm } from "@/api/receipt/types";
 import { getReceiptPage, exportExcel } from "@/api/receipt/index";
 import { genChartByAiUsingPOST } from "@/api/bi";
 import { BiResponse, genChartByAiUsingPOSTParams } from "@/api/bi/types";
-import { SecondaryCategoryQuery } from "@/api/good/types";
-import { reactive } from "vue";
+import { UploadInstance, UploadUserFile } from "element-plus";
 
 const loading = ref(false);
 const ids = ref([]);
@@ -51,17 +50,7 @@ const chartTypes = [
   { value: "饼图", label: "饼图" },
   { value: "雷达图", label: "雷达图" },
 ];
-const userList2 = ref(null);
-// const total1 = reactive({
-//   number: 0,
-// });
-// const total2 = reactive({
-//   number: 0,
-// });
-// const total3 = reactive({
-//   number: 0,
-// });
-const formData1 = reactive<ReceiptForm>({});
+
 const formData = reactive<genChartByAiUsingPOSTParams>({
   creator: "admin",
   goal: "分析",
@@ -75,43 +64,46 @@ const res = reactive<BiResponse>({
 });
 let chartOption = ref();
 const submitting = ref(false);
-
+const fileList = ref<UploadUserFile[]>();
 function handleExport() {
   exportExcel().then((response: any) => {
-    // const blob = new Blob([response.data], {
-    // 	type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8",
-    // });
-    // const a = document.createElement("a");
-    // const href = window.URL.createObjectURL(blob); // 下载的链接
-    // a.href = href;
-    // a.download = decodeURI(
-    // 	response.headers["content-disposition"].split(";")[1].split("=")[1]
-    // ); // 获取后台设置的文件名称
-    // document.body.appendChild(a);
-    // a.click(); // 点击导出
-    // document.body.removeChild(a); // 下载完成移除元素
-    // window.URL.revokeObjectURL(href); // 释放掉blob对象
-    submitting.value = true;
-    genChartByAiUsingPOST(response.data, <genChartByAiUsingPOSTParams>formData)
-      .then(({ data }) => {
-        console.warn(data);
-        if (data.genChart == null) {
-          ElMessage.warning("分析失败");
-        } else {
-          res.chartId = data.chartId;
-          res.genChart = data.genChart;
-          res.genResult = data.genResult;
-          chartOption = JSON.parse(data.genChart);
-          if (!chartOption) {
-            ElMessage.warning("图表代码解析错误");
-          }
-        }
-      })
-      .finally(() => {
-        submitting.value = false;
-      });
+    const blob = new Blob([response.data], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8",
+    });
+    const a = document.createElement("a");
+    const href = window.URL.createObjectURL(blob); // 下载的链接
+    a.href = href;
+    a.download = decodeURI(
+      response.headers["content-disposition"].split(";")[1].split("=")[1]
+    ); // 获取后台设置的文件名称
+    document.body.appendChild(a);
+    a.click(); // 点击导出
+    document.body.removeChild(a); // 下载完成移除元素
+    window.URL.revokeObjectURL(href); // 释放掉blob对象
   });
 }
+
+// function submit(option) {
+// 	submitting.value = true;
+// 	genChartByAiUsingPOST(option.file, <genChartByAiUsingPOSTParams>formData)
+// 		.then(({ data }) => {
+// 			console.warn(data);
+// 			if (data.genChart == null) {
+// 				ElMessage.warning("分析失败");
+// 			} else {
+// 				res.chartId = data.chartId;
+// 				res.genChart = data.genChart;
+// 				res.genResult = data.genResult;
+// 				chartOption = JSON.parse(data.genChart);
+// 				if (!chartOption) {
+// 					ElMessage.warning("图表代码解析错误");
+// 				}
+// 			}
+// 		})
+// 		.finally(() => {
+// 			submitting.value = false;
+// 		});
+// }
 
 function handleQuery1() {
   loading.value = true;
@@ -127,8 +119,6 @@ function handleQuery1() {
 }
 
 function resetQuery1() {
-  // firstCategoryFormRef.value.resetFields();
-
   queryParams1.pageNum = 1;
   handleQuery1();
 }
@@ -137,9 +127,7 @@ function handleSelectionChange(selection: any) {
   ids.value = selection.map((item: any) => item.id);
 }
 
-function resetForm1() {
-  formData1.id = undefined;
-}
+function resetForm1() {}
 
 onMounted(() => {
   handleQuery1();
@@ -153,40 +141,38 @@ onMounted(() => {
       <el-col :span="12">
         <div class="search-container">
           <el-form ref="queryFormRef1" :model="formData" :inline="true">
-            <el-form-item label="分析目标" :rules="formRules.goal">
-              <el-input
-                type="textarea"
-                placeholder="请输入你的分析需求，比如：分析网站用户的增长情况"
-                v-model="formData.goal"
-              ></el-input>
-            </el-form-item>
-            <el-form-item label="图表名称">
-              <el-input
-                placeholder="请输入图表名称"
-                v-model="formData.name"
-              ></el-input>
-            </el-form-item>
-            <el-form-item label="图表类型">
-              <el-select v-model="formData.chartType">
-                <el-option
-                  v-for="option in chartTypes"
-                  :key="option.value"
-                  :value="option.value"
-                  :label="option.label"
-                ></el-option>
-              </el-select>
+            <!--            <el-form-item label="分析目标" :rules="formRules.goal">-->
+            <!--              <el-input-->
+            <!--                type="textarea"-->
+            <!--                placeholder="请输入你的分析需求，比如：分析网站用户的增长情况"-->
+            <!--                v-model="formData.goal"-->
+            <!--              ></el-input>-->
+            <!--            </el-form-item>-->
+            <!--            <el-form-item label="图表名称">-->
+            <!--              <el-input-->
+            <!--                placeholder="请输入图表名称"-->
+            <!--                v-model="formData.name"-->
+            <!--              ></el-input>-->
+            <!--            </el-form-item>-->
+            <!--            <el-form-item label="图表类型">-->
+            <!--              <el-select v-model="formData.chartType">-->
+            <!--                <el-option-->
+            <!--                  v-for="option in chartTypes"-->
+            <!--                  :key="option.value"-->
+            <!--                  :value="option.value"-->
+            <!--                  :label="option.label"-->
+            <!--                ></el-option>-->
+            <!--              </el-select>-->
+            <!--            </el-form-item>-->
+
+            <el-form-item>
+              <el-button @click="resetQuery1"> <i-ep-refresh />重置</el-button>
             </el-form-item>
 
             <el-form-item>
-              <el-button @click="resetQuery1">
-                <i-ep-refresh />
-                重置</el-button
-              >
-            </el-form-item>
-            <el-form-item>
               <el-button class="ml-3" @click="handleExport"
                 ><template #icon><i-ep-download /></template
-                >导出并分析</el-button
+                >导出EXCEL</el-button
               >
             </el-form-item>
           </el-form>
@@ -204,7 +190,6 @@ onMounted(() => {
             :data="userList1"
             @selection-change="handleSelectionChange"
           >
-            <!--            <el-table-column type="selection" width="100" />-->
             <el-table-column
               key="goodName"
               label="货物名称"
@@ -232,17 +217,18 @@ onMounted(() => {
       <!--      <el-col :span="12">-->
       <!--        <div :id="id" :class="className" :style="{ height, width }" />-->
       <!--      </el-col>-->
+
+      <!--    <el-col :span="12">-->
+      <!--      <el-card header="分析结论">-->
+      <!--        <div v-loading="submitting">{{ res.genResult }}</div>-->
+      <!--      </el-card>-->
+      <!--      <el-divider></el-divider>-->
+      <!--      <el-card header="可视化图表" body-style="{ height: '200px' }">-->
+      <!--        <div v-loading="submitting" style="height: 400px">-->
+      <!--          <ECharts ref="chart" :option="chartOption" />-->
+      <!--        </div>-->
+      <!--      </el-card>-->
+      <!--    </el-col>-->
     </el-row>
-    <el-col :span="12">
-      <el-card header="分析结论">
-        <div v-loading="submitting">{{ res.genResult }}</div>
-      </el-card>
-      <el-divider></el-divider>
-      <el-card header="可视化图表" body-style="{ height: '200px' }">
-        <div v-loading="submitting" style="height: 400px">
-          <ECharts ref="chart" :option="chartOption" />
-        </div>
-      </el-card>
-    </el-col>
   </div>
 </template>
